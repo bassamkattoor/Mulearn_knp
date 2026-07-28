a04aef747422fbc10f0df3a08849b863071fea88
bassamkattoor <bassamkattoor@gmail.com>
2026-07-29 00:32:22 +0530

    feat: add smooth Framer Motion page transitions between routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import ScrollToTop from './components/layout/ScrollToTop';
+import PageTransition from './components/layout/PageTransition';
 import Home from './pages/Home';
 import About from './pages/About';
 import Execom from './pages/Execom';
@@ -9,22 +11,32 @@ import Leaderboard from './pages/Leaderboard';
 import Join from './pages/Join';
 import Theory from './pages/Theory';
 
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageTransition><Home /></PageTransition>} />
+        <Route path="/about" element={<PageTransition><About /></PageTransition>} />
+        <Route path="/execom" element={<PageTransition><Execom /></PageTransition>} />
+        <Route path="/events" element={<PageTransition><Events /></PageTransition>} />
+        <Route path="/notice-board" element={<PageTransition><NoticeBoard /></PageTransition>} />
+        <Route path="/leaderboard" element={<PageTransition><Leaderboard /></PageTransition>} />
+        <Route path="/join" element={<PageTransition><Join /></PageTransition>} />
+        <Route path="/theory" element={<PageTransition><Theory /></PageTransition>} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 export default function App() {
   return (
     <Router>
       <div className="min-h-screen bg-background text-textMain font-sans flex flex-col justify-between">
         <ScrollToTop />
         <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/execom" element={<Execom />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/notice-board" element={<NoticeBoard />} />
-            <Route path="/leaderboard" element={<Leaderboard />} />
-            <Route path="/join" element={<Join />} />
-            <Route path="/theory" element={<Theory />} />
-          </Routes>
+          <AnimatedRoutes />
         </main>
       </div>
     </Router>
